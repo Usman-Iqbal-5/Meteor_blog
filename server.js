bc86fab4bf9c7c9847ef005f241dede40c8b85b9
Usman Iqbal <usmaniqbal912@outlook.com>
2025-10-15 21:31:33 +0100

    404 error handler

--- server.js
+++ server.js
@@ -108,6 +108,10 @@ app.delete("/delete/:id", async (req, res) => {
   }
 });
 
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).send('404 Page Not Found' );
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
