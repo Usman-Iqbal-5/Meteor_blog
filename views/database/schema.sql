e1b3ed9952229b4c2b8fbdaf6aced54bd9b2066a
usman-Iqbal-5 <usmaniqbal912@outlook.com>
2026-04-09 00:55:54 +0100

    improved folder structure

--- views/database/schema.sql
+++
@@ -1,7 +0,0 @@
--- blog_posts table
-CREATE TABLE IF NOT EXISTS blog_posts (
-    blog_id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    summary TEXT NOT NULL,
-    main_content TEXT NOT NULL
-);
